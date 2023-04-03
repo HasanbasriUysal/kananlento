8eee007945edeff770fc8ea740bbb3d42ee64768
HasanbasriUysal <48217@edu.raseko.fi>
2023-04-03 13:01:09 +0300

    Muuta luokaksi

--- game.py
+++ game.py
@@ -1,24 +1,41 @@
 import pygame
 
 def main():
-    screen = pygame.display.set_mode(800,600)
-    clock = pygame.time.Clock()
+    game = Game()
+    game.run()
 
-    running = True
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((800,600))
+        self.running = False
 
-    while running:
+    def run(self):        
+        clock = pygame.time.Clock()
+        self.running = True
+        while self.running:
+            self.handle_events()
+            self.handle_game_logic()
+            self.update_screen()
+            #Odata niin kauan, etta ruudun paivytysnopeus on 60fps
+            clock.tick(60) 
+
+        pygame.quit()
+
+    def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                self.running = False
 
-        screen.fill("purple")
-        
+    def handle_game_logic(self):
+        pass
+
+
+    def update_screen(self):
+        self.screen.fill((230,230,255))        
         pygame.display.flip()
 
-      
 
-        clock.tick(60) #Odata niin kauan, etta ruudun paivytysnopeus on 60fps
-  
 
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
